58208646056dd226b2777324d54e4b8a545ddf02
santosh <santosh.gond@aptaracorp.com>
2025-09-09 12:44:46 +0530

    bug fixed

--- users/models.py
+++ users/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import os
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    image = models.ImageField(upload_to="profile_pics", null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} Profile"
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # If image exists and file is accessible
+
+         # Resize only if an image was uploaded
+        if self.image and os.path.exists(self.image.path):
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
+
